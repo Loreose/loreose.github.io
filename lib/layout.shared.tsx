f3449f3cef7d11af8478c8eb0ad9702731cfc9b6
Loreose <yyagizozcicekk@gmail.com>
2026-09-22 16:46:00 +0300

    fixed logos scale

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -11,13 +11,13 @@ export function baseOptions(): BaseLayoutProps {
           <img
             src="/logo-dark.png"
             alt={appName}
-            className="h-6 w-auto block dark:hidden"
+            className="h-10 w-auto block dark:hidden"
           />
           {/* Dark Theme Logo */}
           <img
             src="/logo-light.png"
             alt={appName}
-            className="h-6 w-auto hidden dark:block"
+            className="h-10 w-auto hidden dark:block"
           />
           <span>{appName}</span>
         </div>
