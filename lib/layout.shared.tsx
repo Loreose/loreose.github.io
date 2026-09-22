01084a4b276f44e8071e7ebab1df59495df30338
Loreose <yyagizozcicekk@gmail.com>
2026-09-22 17:00:17 +0300

    added navbar to buttons

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -24,5 +24,65 @@ export function baseOptions(): BaseLayoutProps {
       ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
+    links: [
+      {
+        type: "main",
+        text: "Documentation",
+        url: "/docs",
+        on: "nav",
+      },
+      {
+        type: "icon",
+        label: "Tebex",
+        icon: (
+          <div className="flex items-center justify-center">
+            {/* Light Theme Tebex Icon */}
+            <img
+              src="/tebex-dark.png"
+              alt="Tebex"
+              width={22}
+              height={22}
+              className="opacity-75 hover:opacity-100 block dark:hidden"
+            />
+            {/* Dark Theme Tebex Icon */}
+            <img
+              src="/tebex-light.png"
+              alt="Tebex"
+              width={22}
+              height={22}
+              className="opacity-75 hover:opacity-100 hidden dark:block"
+            />
+          </div>
+        ),
+        text: "Tebex Store",
+        url: "https://your-store.tebex.io",
+      },
+      {
+        type: "icon",
+        label: "Discord",
+        icon: (
+          <div className="flex items-center justify-center">
+            {/* Light Theme Discord Icon */}
+            <img
+              src="/discord-dark.png"
+              alt="Discord"
+              width={22}
+              height={22}
+              className="opacity-75 hover:opacity-100 block dark:hidden"
+            />
+            {/* Dark Theme Discord Icon */}
+            <img
+              src="/discord-light.png"
+              alt="Discord"
+              width={22}
+              height={22}
+              className="opacity-75 hover:opacity-100 hidden dark:block"
+            />
+          </div>
+        ),
+        text: "Discord",
+        url: "https://discord.gg/your-server",
+      },
+    ],
   };
 }
